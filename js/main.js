be6a0e0736f1bc2bda6dfc68327473d581d867a2
HyunJae <hjstylelu@gmail.com>
2022-01-30 12:09:02 +0900

    2021.01.30 add some contents

--- js/main.js
+++ js/main.js
@@ -11,7 +11,28 @@ searchInputEl.addEventListener("focus", function () {
 });
 
 searchInputEl.addEventListener("blur", function () {
-  [];
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      //배지 숨기기
+      //gsap.to(요소, 지속시간, 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
